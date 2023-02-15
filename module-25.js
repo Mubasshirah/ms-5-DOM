d927ae331784ded3b58ca81ccf676011692488d8
Mubasshirah <adibaju1994@gmail.com>
2023-02-15 22:52:28 +0600

    25

---
+++ module-25.js
@@ -0,0 +1,40 @@
+// console.log(document.getElementsByTagName('li'))
+// const getLi=document.getElementsByTagName('li');
+// for(const li of getLi){
+//     console.log(li.innerText);
+// }
+// console.log(document.getElementById('p-tag'));
+// console.log(document.getElementsByClassName('h1-tag'));
+// const allLi=document.querySelectorAll('.nav-section li');
+// for(const li of allLi){
+//     console.log(li.innerText)
+// console.log(document.getElementsByClassName('h1-tag').style.color='red')
+// const sections=document.querySelectorAll('section');
+// console.log(sections);
+// for(const section of sections){
+//     section.style.border='2px solid blue';
+//     section.style.marginBottom='20px';
+//     section.style.backgroundColor='lightgray';
+// }
+// const h4Button=document.getElementById('h4-button');
+// console.log(h4Button);
+// h4Button.classList.add('h4-button');
+// h4Button.classList.remove('h4-button')
+
+// const h4Button=document.getElementById('h4-button')
+// console.log(h4Button.childNodes[3])
+// const placeSul=document.getElementById('placeSul');
+
+// const li=document.createElement('li');
+// li.innerText='gallery';
+
+// placeSul.appendChild(li);
+// console.log(placeSul)
+// console.log(placeSul.parentNode)
+// const mainContainer=document.getElementById('mainContainer');
+// const newSection=document.createElement('section');
+// newSection.innerHTML=`
+// <h2>new section</h2>
+// <p>hfhsdjhjsdahfahfiahfaihfisduhiuhaiud</p>
+// `
+// const newSectionappend=mainContainer.appendChild(newSection);
