f4c384533d8a37ae47825eb996078971a1d85ab5
Mubasshirah <adibaju1994@gmail.com>
2023-02-16 22:59:28 +0600

    g

---
+++ module-26.js
@@ -0,0 +1,55 @@
+// function makeRed(){
+//     document.body.style.backgroundColor='red';
+// }
+// function removeRed(){
+//     document.body.style.backgroundColor='';
+// }
+// const makeBlue=document.getElementById('blue');
+// makeBlue.onclick=makeblue;
+// function makeblue(){
+//     document.body.style.backgroundColor='blue';
+// }
+// const makePink=document.getElementById('pink');
+// makePink.addEventListener('click', function makepink(){
+//                document.body.style.backgroundColor='pink';
+// })
+// document.getElementById('violet').addEventListener('click', function makeViolet(){defaultText
+//     document.body.style.backgroundColor='violet';
+// })
+// document.getElementById('changeDefaultText').addEventListener('click',function(){
+//     const defaultText=document.getElementById('defaultText');
+//     defaultText.innerText='i have changed the default text';
+// })
+// document.getElementById('inputButton').addEventListener('click',function(){
+//     const inputtext=document.getElementById('inputText');
+//     const inputtextvalue=inputtext.value;
+//     const seconddefault=document.getElementById('secondDefault');
+//     // seconddefault.innerText='second default text has been changed';
+//     seconddefault.innerText=inputtextvalue;
+//     inputtext.value ='';
+
+// })
+// document.getElementById('buttonForPost').addEventListener('click',function(){
+//     const textArea=document.getElementById('newComment');
+//     const textAreaValue=textArea.value;
+//     const commentsection=document.getElementById('commentSection');
+//     const p=document.createElement('p');
+//     p.innerText=textAreaValue;
+//     commentsection.appendChild(p);
+//     textArea.value='';
+// })
+// document.getElementById('inputedText').addEventListener('keyup',function(event){
+//     const inputedtext=event.target.value;
+//     const deleteButton=document.getElementById('deleteButton');
+//     if(inputedtext=='delete'){
+// deleteButton.removeAttribute('disabled');
+//     }
+//     else{
+//         deleteButton.setAttribute('disabled',true);
+//     }
+// })
+// document.getElementById('deleteButton').addEventListener('click',function(){
+//  const deletedtext=document.getElementById('deletedText');
+//  deletedtext.style.display='none';
+// })
+
